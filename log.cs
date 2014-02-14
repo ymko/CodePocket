b2b5d8b540f34ca184264deb3547762e1fb099ae
ymko <ymko@ymko.co.jp>
2014-02-15 01:46:24 +0900

    ログローテート追加

--- log.cs
+++ log.cs
@@ -19,6 +19,7 @@
 //		http://msdn.microsoft.com/ja-jp/library/ms404278(v=vs.110).aspx
 //		方法 : ログ ファイルを開いて情報を追加する
 //		http://msdn.microsoft.com/ja-jp/library/3zc0w663(v=vs.110).aspx
+//	ログローテート
 
 namespace Main
 {
@@ -30,7 +31,7 @@ namespace Main
 		static void Main()
 		{
 			WriteLog("日本語SJIS");
-			WriteLog("てすと");
+			//WriteLog("てすと");
 		}
 		
 		private static void WriteLog(String msg)
@@ -42,10 +43,44 @@ namespace Main
 
 			string format = DateTime.Now.ToString() + " " + msg;
 
+			// 書き込み
 			using (StreamWriter w = new StreamWriter(
 				app_log, true, System.Text.Encoding.GetEncoding("shift_jis")))
 			{
 				w.WriteLine(format);
+			}
+
+			// ローテート
+			int maxSize = 1 * 1024 * 1024;
+			int maxFile = 5;
+
+			FileInfo fi = new FileInfo(app_log);
+			if (fi.Length > maxSize) {
+
+				// 4->5,3->2,2->1
+				string src, dst;
+				for (int i = maxFile; 0 < i; i--)
+				{
+					if (i == maxFile) continue;
+					src = String.Format("{0:S}.{1:D}", app_log, i);
+					dst = String.Format("{0:S}.{1:D}", app_log, i + 1);
+
+					if (! File.Exists(src)) continue;
+
+					FileInfo srcfi = new FileInfo(src);
+					srcfi.CopyTo(dst, true);// 上書き許可
+				}
+				// .log->1
+				src = String.Format("{0:S}", app_log);
+				dst = String.Format("{0:S}.{1:D}", app_log, 1);
+				fi.CopyTo(dst, true);
+
+				// 切り詰める
+				using (FileStream fs = new FileStream(
+					app_log, FileMode.Truncate))
+				{
+					;
+				}
 			}
 		}
 	}
